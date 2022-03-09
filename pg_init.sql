b59d192dd915893f17c79b75f24189951c613cc0
Viljem Skornik <vili@global.id>
2022-03-09 09:25:32 -0800

    Add pg extension

--- pg_init.sql
+++ pg_init.sql
@@ -2,3 +2,8 @@ CREATE USER identity WITH PASSWORD 'passpass';
 CREATE USER contacts WITH PASSWORD 'blanks';
 CREATE DATABASE identity OWNER identity;
 CREATE DATABASE contacts OWNER contacts;
+
+# Not sure how to create the exentions in specific databases, yet.
+# Run manually for the database that needs it.
+# CREATE EXTENSION IF NOT EXISTS pg_trgm;
+
