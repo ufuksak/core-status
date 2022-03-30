d3de7b95df5a07a8b201fb61b6b294637eba2ae6
Yuri Yefimofich <yuri@backendless.consulting>
2022-03-30 20:07:49 +0300

    CPSD-44: ignore duplicated statuses

--- src/products/repositories/status.repository.ts
+++ src/products/repositories/status.repository.ts
@@ -5,19 +5,19 @@ import { StatusEntity } from "../entity/status.entity";
 @EntityRepository(StatusEntity)
 export class StatusRepository extends Repository<StatusEntity> {
 
-  saveUser = async (userDto: StatusDto) => {
-    await this.save(userDto);
+  saveStatus = async (statusDto: StatusDto) => {
+    await this.save(statusDto);
   }
 
-  getUsers = async () => {
+  getStatuses = async () => {
     return await this.find();
   }
 
-  getUserById = async (id: string, options?: FindOneOptions<StatusEntity>) => {
+  getStatusById = async (id: string, options?: FindOneOptions<StatusEntity>) => {
     return await this.findOneOrFail(id, options);
   }
 
-  deleteUser = async (id: string) => {
+  deleteStatus = async (id: string) => {
     return await this.delete(id);
   }
 }
