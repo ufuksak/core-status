4c6e28ae06f57d5fab13a16b0db12e4f4245fb60
ufuksakar <ufuksakar@gmail.com>
2022-05-17 00:40:37 +0300

    STDB-60: implementations

--- src/products/services/subscribers.service.ts
+++ src/products/services/subscribers.service.ts
@@ -1,67 +1,96 @@
-import {Injectable, InternalServerErrorException, Logger} from "@nestjs/common";
-import {InjectRepository} from "@nestjs/typeorm";
-import {UpdateEntity} from "../entity/update.entity";
-import {StatusPublisher} from "../rabbit/status.publisher";
-import {StreamRepository} from "../repositories/stream.repository";
-import {GrantType} from "../dto/grant.model";
-import {GrantEntity} from "../entity/grant.entity";
-import {CHANNEL_PREFIX} from "../../../worker/src/services/pubnub.service";
 import {
-  addChannelsToPubnubChannelGroup, pubnubPublish,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { UpdateEntity } from "../entity/update.entity";
+import { StatusPublisher } from "../rabbit/status.publisher";
+import { StreamRepository } from "../repositories/stream.repository";
+import { GrantType } from "../dto/grant.model";
+import { GrantEntity } from "../entity/grant.entity";
+import { CHANNEL_PREFIX } from "../../../worker/src/services/pubnub.service";
+import {
+  addChannelsToPubnubChannelGroup,
+  pubnubPublish,
   removeChannelsFromPubnubChannelGroup,
-  subscribeToChannel, unsubscribeFromChannel
+  subscribeToChannel,
+  unsubscribeFromChannel,
 } from "../pubnub/pubnub";
 import {
   ChannelGroupAdditionError,
   ChannelGroupRemovalError,
-  PushNotificationSendingError
+  PushNotificationSendingError,
 } from "../exception/response.exception";
-import {PubnubNotification} from "../pubnub/notification";
+import { PubnubNotification } from "../pubnub/notification";
 
 @Injectable()
 export class SubscribersService {
   private readonly logger = new Logger(SubscribersService.name);
   constructor(
-    @InjectRepository(StreamRepository) private readonly streamRepo: StreamRepository,
-    private readonly statusPublisher: StatusPublisher,
+    @InjectRepository(StreamRepository)
+    private readonly streamRepo: StreamRepository,
+    private readonly statusPublisher: StatusPublisher
   ) {}
 
   async pushToWorker(update: UpdateEntity) {
-    const {stream_id} = update;
+    const { stream_id } = update;
 
-    const stream = await this.streamRepo.findOne(stream_id, {relations: ['grants', 'streamType']});
-    if(!stream) {
-      this.logger.error(`received update ${update.id} for unknown stream ${stream_id}`)
-      return
+    const stream = await this.streamRepo.findOne(stream_id, {
+      relations: ["grants", "streamType"],
+    });
+    if (!stream) {
+      this.logger.error(
+        `received update ${update.id} for unknown stream ${stream_id}`
+      );
+      return;
     }
 
-    const {id, recorded_at, payload} = update;
+    const { id, recorded_at, payload } = update;
 
-    for(const grant of stream.grants) {
-      if(grant.type === GrantType.all || grant.type === GrantType.latest) {
-        const {id: grant_id, owner_id: user_id, recipient_id, properties} = grant;
-        await this.statusPublisher.publishStatusUpdate({
-          id,
-          stream_id,
-          grant_id,
-          user_id,
+    for (const grant of stream.grants) {
+      if (grant.type === GrantType.all || grant.type === GrantType.latest) {
+        const {
+          id: grant_id,
+          owner_id: user_id,
           recipient_id,
-          recorded_at: new Date(recorded_at).toISOString(),
-          payload,
-          reEncryptionKey: properties.reEncryptionKey
-        }).catch(e => this.logger.error(`error while publish updates to rabbit ${e.message}`));
+          properties,
+        } = grant;
+        await this.statusPublisher
+          .publishStatusUpdate({
+            id,
+            stream_id,
+            grant_id,
+            user_id,
+            recipient_id,
+            recorded_at: new Date(recorded_at).toISOString(),
+            payload,
+            reEncryptionKey: properties.reEncryptionKey,
+          })
+          .catch((e) =>
+            this.logger.error(
+              `error while publish updates to rabbit ${e.message}`
+            )
+          );
       }
     }
   }
 
-  async pushGrantToChannelGroup(notification : PubnubNotification, update: GrantEntity) {
-    if(update) {
-      const {stream_id} = update;
+  async pushGrantToChannelGroup(
+    notification: PubnubNotification,
+    update: GrantEntity
+  ) {
+    if (update) {
+      const { stream_id } = update;
 
-      const stream = await this.streamRepo.findOne(update.stream_id, {relations: ['grants']});
+      const stream = await this.streamRepo.findOne(update.stream_id, {
+        relations: ["grants"],
+      });
       if (!stream) {
-        this.logger.error(`received update ${update.id} for unknown stream ${stream_id}`)
-        return
+        this.logger.error(
+          `received update ${update.id} for unknown stream ${stream_id}`
+        );
+        return;
       }
 
       const channelName = CHANNEL_PREFIX + update.id;
@@ -70,31 +99,42 @@ export class SubscribersService {
         grantChannelArray.push(channelName);
         const grantChannelGroup = `${stream.type}_${update.recipient_id}`;
         await subscribeToChannel(grantChannelArray);
-        await addChannelsToPubnubChannelGroup(grantChannelArray, grantChannelGroup);
+        await addChannelsToPubnubChannelGroup(
+          grantChannelArray,
+          grantChannelGroup
+        );
       } catch (e) {
-        this.logger.error(`error while adding to the channel group ${e.message}`);
+        this.logger.error(
+          `error while adding to the channel group ${e.message}`
+        );
         throw new ChannelGroupAdditionError();
       }
-      
+
       try {
         if (update.type === GrantType.latest) {
           await pubnubPublish(channelName, notification);
         }
       } catch (e) {
-        this.logger.error(`error while creating push notifications for ${channelName} channel. ${e.message}`);
+        this.logger.error(
+          `error while creating push notifications for ${channelName} channel. ${e.message}`
+        );
         throw new PushNotificationSendingError();
       }
     }
   }
 
   async removeFromChannelGroup(update: GrantEntity) {
-    if(update) {
-      const {stream_id} = update;
+    if (update) {
+      const { stream_id } = update;
 
-      const stream = await this.streamRepo.findOne(update.stream_id, {relations: ['grants']});
+      const stream = await this.streamRepo.findOne(update.stream_id, {
+        relations: ["grants"],
+      });
       if (!stream) {
-        this.logger.error(`received update ${update.id} for unknown stream ${stream_id}`)
-        return
+        this.logger.error(
+          `received update ${update.id} for unknown stream ${stream_id}`
+        );
+        return;
       }
 
       try {
@@ -102,9 +142,14 @@ export class SubscribersService {
         grantChannelArray.push(CHANNEL_PREFIX + update.id);
         const grantChannelGroup = `${stream.type}_${update.recipient_id}`;
         await unsubscribeFromChannel(grantChannelArray);
-        await removeChannelsFromPubnubChannelGroup(grantChannelArray, grantChannelGroup);
+        await removeChannelsFromPubnubChannelGroup(
+          grantChannelArray,
+          grantChannelGroup
+        );
       } catch (e) {
-        this.logger.error(`error while adding to the channel group ${e.message}`);
+        this.logger.error(
+          `error while adding to the channel group ${e.message}`
+        );
         throw new ChannelGroupRemovalError();
       }
     }
