55afe06820803a8fcc8d8ebdbf57ada947045ced
ufuksakar <ufuksakar@gmail.com>
2022-05-17 20:16:49 +0300

    STDB-60: revert repository impl

--- src/products/repositories/grant.repository.ts
+++ src/products/repositories/grant.repository.ts
@@ -1,23 +1,23 @@
-import {EntityRepository, Repository} from "typeorm";
+import {EntityRepository, FindOneOptions, Repository} from "typeorm";
 import { GrantDto } from "../dto/grant.model";
-import {GrantEntity} from "../entity/grant.entity";
+import { GrantEntity } from "../entity/grant.entity";
 
 @EntityRepository(GrantEntity)
 export class GrantRepository extends Repository<GrantEntity> {
 
-    saveGrant(grantDto: GrantDto): Promise<GrantEntity> {
-        return this.save(grantDto);
-    }
+  saveGrant(grantDto: GrantDto): Promise<GrantEntity> {
+    return this.save(grantDto);
+  }
 
-    getGrants = async () => {
-        return await this.find();
-    }
+  getGrants(): Promise<GrantEntity[]>{
+    return this.find();
+  }
 
-    getGrantById = async (id: string) => {
-        return await this.findOneOrFail(id);
-    }
+  getGrantById(id: string, options?: FindOneOptions<GrantEntity>): Promise<GrantEntity> {
+    return this.findOneOrFail(id, options);
+  }
 
-    deleteGrant = async (id: string) => {
-        return await this.delete(id);
-    }
+  deleteGrant(id: string) {
+    return this.delete(id);
+  }
 }
